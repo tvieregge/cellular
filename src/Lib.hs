b0dd187e23fc509ee3428ecc93dd4fe39bdec5d2
tvieregge <tmvieregge@gmail.com>
2019-06-26 12:01:41 -0400

    using strategy from http://blog.emillon.org/posts/2012-10-18-comonadic-life.html

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,38 +5,49 @@ module Lib
 import Control.Comonad
 import Control.Monad
 
+data CellData = CB Bool | CI Int deriving(Show)
+
 data Zipper x = Zipper [x] x [x] deriving(Show)
 data U x = U (Zipper (Zipper x)) deriving(Show)
 
 someFunc :: IO ()
-someFunc = mconcat $ map (display 10) (take 20 $ iterate (extend rule) makeGrid)
+someFunc =  putStrLn $ display $ result 1 rule makeGlider
 
--- rule :: Zipper Bool -> Bool
--- rule (Zipper (l:ls) x (r:rs)) = r==l
+display ::  U CellData -> String
+display (U z) = unlines $ map toString $ toList $ fmap toList z
 
-rule :: U Bool -> Bool
+toString :: [CellData] -> String
+toString cd = mconcat $ map displayCell cd
+
+displayCell :: CellData -> String
+displayCell (CI n) = show n
+displayCell (CB b) = case b of
+                       True -> "*"
+                       False -> "-"
+
+result :: Int -> (U a -> a) -> U a -> U a
+result steps rule u = last . take steps $ iterate (extend rule) u
+
+toList :: Zipper a -> [a]
+toList (Zipper ls f rs) = reverse (take 10 ls) ++ [f] ++ (take 10 rs)
+
+rule :: U a -> a
 rule u = extract u
-    -- where z = (Zipper (l:ls) x (r:rs))
-
-display :: Int -> U Bool -> IO ()
-display n (U z) = extract $ fmap (displayZ n) z
--- display n u = extract $ fmap (displayZ n) u
-
-displayZ :: Int -> Zipper Bool -> IO ()
-displayZ n (Zipper l f r) = putStrLn .
-    fmap vis $
-    (reverse (take n l)) ++ [f] ++ (take n r)
-    where vis True = '*'
-          vis False = ' '
 
 makeLine :: Zipper Bool
 makeLine = Zipper (repeat False) True (repeat False)
+
+makeIntGrid :: U CellData
+makeIntGrid = U $ Zipper (repeat (shiftLeft z)) z (repeat z)
+    where z = Zipper left (CI 0) right
+          left = (map CI $ iterate (subtract 1) (-1))
+          right = (map CI [1 ..])
 
 makeGrid :: U Bool
 makeGrid = U $ Zipper (repeat z) z (repeat z)
     where z = Zipper (repeat False) True (repeat False)
 
-makeGlider :: U Bool
+makeGlider :: U CellData
 makeGlider =
     U $ Zipper (repeat fz) fz rs
     where
@@ -44,14 +55,21 @@ makeGlider =
              , line [f, f, t]
              , line [t, t, t]
              ] ++ repeat fz
-        t = True
-        f = False
+        t = CB True
+        f = CB False
         fl = repeat f
         fz = Zipper fl f fl
         line l = Zipper fl f (l ++ fl)
 
 instance Functor Zipper where
     fmap f (Zipper ls x rs) = Zipper (fmap f ls) (f x) (fmap f rs)
+
+shift :: (z a -> z a)
+      -> (z a -> z a)
+      -> z a
+      -> Zipper (z a)
+shift l r z =
+    Zipper (iterate' l z) z (iterate' r z)
 
 iterate' :: (a -> a) -> a -> [a]
 iterate' f a = tail $ iterate f a
@@ -66,9 +84,28 @@ instance Comonad Zipper where
     extract (Zipper ls x rs) = x
     duplicate z = Zipper (iterate' shiftLeft z) z (iterate' shiftRight z)
 
+up :: U a -> U a
+up (U z) = U (shiftLeft z)
+
+down :: U a -> U a
+down (U z) = U (shiftRight z)
+
+left :: U a -> U a
+left (U z) = U (fmap shiftLeft z)
+
+right :: U a -> U a
+right (U z) = U (fmap shiftRight z)
+
+horizontal :: U a -> Zipper (U a)
+horizontal = shift left right
+
+vertical :: U a ->  Zipper (U a)
+vertical = shift up down
+
 instance Functor U where
     fmap f (U z) = U $ (fmap . fmap) f z
 
 instance Comonad U where
     extract (U z) = extract $ extract z
-    duplicate (U z) = fmap U . U . duplicate $ duplicate z
+    duplicate u = U $ fmap horizontal $ vertical u
+    -- duplicate (U z) = fmap U . U . duplicate $ duplicate z
