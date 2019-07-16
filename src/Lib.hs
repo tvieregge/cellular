32f19e0bb2d09cb88e75d10ad3e35e4dbe61590e
tvieregge <tmvieregge@gmail.com>
2019-07-16 13:46:16 -0400

    Splitting Zipper and NZipper into seperate file

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,30 +4,11 @@ module Lib
 
 import Control.Comonad
 import Control.Monad
+import NZipper
 
 data CellData = CB Bool | CI Int deriving(Show)
 
-data Zipper x = Zipper [x] x [x] deriving(Show)
 data U x = U (Zipper (Zipper x)) deriving(Show)
-
--- n-dimentional zippers
-
-data NZipper a = Z a | S (NZipper (Zipper a)) deriving Show
--- data NZipper a = Z a | S (NZipper [a]) deriving Show
-
-makeList :: Int -> a -> NZipper a
-makeList 0 v = S (Z $ Zipper [v,v,v] v [v,v,v])
-makeList n v = S (makeList (n-1) $ Zipper (replicate 3 v) v (replicate 3 v))
-
-instance Functor NZipper where
-    fmap f (Z v) = Z (f v)
-    fmap f (S v) = S (fmap (fmap f) v)
-
-testShift :: NZipper a -> NZipper a
-testShift (Z v) = Z v
-testShift (S v) = S (fmap shiftLeft (testShift v))
-
--- end experiment
 
 someFunc :: IO ()
 someFunc =  mapM_ putStrLn $ map (\x -> display $ result x golRule makeGlider) [1..5]
@@ -48,14 +29,8 @@ displayCell (CB b) = case b of
 result :: Int -> (U a -> a) -> U a -> U a
 result steps rule u = last . take steps $ iterate (extend rule) u
 
-toList :: Zipper a -> [a]
-toList (Zipper ls f rs) = reverse (take 10 ls) ++ [f] ++ (take 10 rs)
-
 rule :: U a -> a
 rule u = extract u
-
-makeLine :: Zipper Bool
-makeLine = Zipper (repeat False) True (repeat False)
 
 makeIntGrid :: U CellData
 makeIntGrid = U $ Zipper (repeat (shiftLeft z)) z (repeat z)
@@ -80,15 +55,6 @@ makeGlider =
         fl = repeat f
         fz = Zipper fl f fl
         line l = Zipper fl f (l ++ fl)
-
-iterate' :: (a -> a) -> a -> [a]
-iterate' f a = tail $ iterate f a
-
-shiftRight (Zipper ls x (r:rs)) = Zipper (x:ls) r rs
-shiftRight _ = error "bad right"
-
-shiftLeft (Zipper (l:ls) x rs) = Zipper ls l (x:rs)
-shiftLeft _ = error "bad left"
 
 up :: U a -> U a
 up (U z) = U (shiftLeft z)
@@ -125,25 +91,8 @@ golRule u = case nCount of
     where nCount = length . filter id $ getBool <$> neighbours u
           getBool (CB b) = b
 
-instance Comonad Zipper where
-    extract (Zipper ls x rs) = x
-    duplicate z = shift shiftLeft shiftRight z
-    -- duplicate z = Zipper (iterate' shiftLeft z) z (iterate' shiftRight z)
-
-instance Functor Zipper where
-    fmap f (Zipper ls x rs) = Zipper (fmap f ls) (f x) (fmap f rs)
-
-shift :: (z a -> z a)
-      -> (z a -> z a)
-      -> z a
-      -> Zipper (z a)
-shift l r z =
-    Zipper (iterate' l z) z (iterate' r z)
-
 instance Functor U where
     fmap f (U z) = U $ (fmap . fmap) f z
-
-getZipper (U z) = z
 
 instance Comonad U where
     extract (U z) = extract $ extract z
@@ -152,3 +101,5 @@ instance Comonad U where
                         (shift (U . shiftLeft . getZipper) (U . shiftRight . getZipper)) u
     -- duplicate u = U $ fmap horizontal $ vertical u
     -- duplicate (U z) = fmap U . U . duplicate $ duplicate z
+
+getZipper (U z) = z
---
+++ src/NZipper.hs
@@ -0,0 +1,60 @@
+module NZipper where
+
+import Control.Comonad
+
+-- Base Zipper
+data Zipper x = Zipper [x] x [x] deriving(Show)
+
+instance Comonad Zipper where
+    extract (Zipper ls x rs) = x
+    duplicate z = shift shiftLeft shiftRight z
+    -- duplicate z = Zipper (iterate' shiftLeft z) z (iterate' shiftRight z)
+
+instance Functor Zipper where
+    fmap f (Zipper ls x rs) = Zipper (fmap f ls) (f x) (fmap f rs)
+
+makeLine :: Zipper Bool
+makeLine = Zipper (repeat False) True (repeat False)
+
+iterate' :: (a -> a) -> a -> [a]
+iterate' f a = tail $ iterate f a
+
+shiftRight (Zipper ls x (r:rs)) = Zipper (x:ls) r rs
+shiftRight _ = error "bad right"
+
+shiftLeft (Zipper (l:ls) x rs) = Zipper ls l (x:rs)
+shiftLeft _ = error "bad left"
+
+shift :: (z a -> z a)
+      -> (z a -> z a)
+      -> z a
+      -> Zipper (z a)
+shift l r z =
+    Zipper (iterate' l z) z (iterate' r z)
+
+toList :: Zipper a -> [a]
+toList (Zipper ls f rs) = reverse (take 10 ls) ++ [f] ++ (take 10 rs)
+
+
+-- N-Dimentional Zippers
+data NZipper a = Z a | S (NZipper (Zipper a)) deriving Show
+
+instance Functor NZipper where
+    fmap f (Z v) = Z (f v)
+    fmap f (S v) = S (fmap (fmap f) v)
+
+instance Comonad NZipper where
+    extract (Z v) = v
+    -- extract (S v) = extract v
+
+makeList :: Int -> a -> NZipper a
+makeList 0 v = S (Z $ Zipper [v,v,v] v [v,v,v])
+makeList n v = S (makeList (n-1) $ Zipper (replicate 3 v) v (replicate 3 v))
+
+testShift :: NZipper a -> NZipper a
+testShift (Z v) = Z v
+testShift (S v) = S (fmap shiftLeft (testShift v))
+
+shift1 :: NZipper a -> NZipper a
+shift1 = id
+
